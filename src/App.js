5ec6313f38971b8bcd8516025970fcd11b27052b
K-Kocak <kakocak@outlook.com>
2024-05-20 05:39:35 +0100

    Small UI Update and code comments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from "react"
 import Die from "./components/Die"
 import {nanoid} from "nanoid"
 
-
-
-
 function App() {
 
+  // setting our states
   const [dice, setDice] = React.useState(allNewDice)
   const [tenzies, setTenzies] = React.useState(false)
 
+  // if all dice are held & they are all the same value, set tenzies to true
+  // this is called every time the state of a dice changes (like clicking on a button)
   React.useEffect(() => {
     const allHeld = dice.every(die => die.isHeld)
     const firstValue = dice[0].value
@@ -21,6 +21,7 @@ function App() {
     }
   }, [dice])
 
+  // remakes a die with a new value
   function generateNewDie() {
     return {
       value: Math.ceil(Math.random() * 6),
@@ -29,6 +30,9 @@ function App() {
     }
   }
 
+  // this is called when a die is clicked, it goes through all the dice
+  // and when it finds the one that has an id equal to the one we clicked,
+  // it reverses its "itHeld" state
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
       return die.id === id ? {...die, isHeld: !die.isHeld} :
@@ -36,6 +40,7 @@ function App() {
     }))
   }
 
+  // generates all the dice anew
   function allNewDice() {
     const newDice = []
     for(let i = 0; i< 10; i++) {
@@ -43,9 +48,10 @@ function App() {
     }
     return newDice
   }
-  
-  
 
+  // for every dice that isnt held, it runs generatedNewDie, else die remains the same
+  // if tenzies is true though, aka you had won, then it sets tenzies to false and starts
+  // a new round.
   function rollDice() {
     if(!tenzies) {
       setDice(oldDice => oldDice.map(die => {
@@ -54,10 +60,10 @@ function App() {
     } else {
       setTenzies(false)
       setDice(allNewDice())
-    }
-    
+    }   
   }
 
+  // this essentially makes our 10 die on the screen using our imported Die.js component
   const diceElements = dice.map(die => <Die key={die.id}
     value={die.value}
     isHeld={die.isHeld}
@@ -67,7 +73,6 @@ function App() {
     <main>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-
       <div className="dice-container">
       {diceElements}
       </div>
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,5 @@
 import React from "react"
 
-
 export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59e391" : "white"
